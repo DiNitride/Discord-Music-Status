dea44f49ad130f0503c0f5948dd0371a9051d6e7
DiNitride <jamesthecookie@gmail.com>
2017-08-11 11:06:05 +0100

    Made everything less terrible

--- README.md
+++ README.md
@@ -3,27 +3,28 @@
 
 The status won't display on your own client for some reason, Discord's fault, however if you look on another client or mobile you see it will be displayed.
 
-###### The program will work with:
-- iTunes
+##### The program will work with:
 - Spotify
-- VLC
+- iTunes
 - Winamp
-- Foobar2000
-- (Any other program Snip is updated to be able to access)
+- foobar2000
+- VLC
+- Google Play Music Desktop Player
+- Quod Libet
 
-###### Requirements:
-- Python 3
-- Discord.py / https://github.com/Rapptz/discord.py
+##### Requirements:
+- Python 3.6
+- Discord.py and Logbook installed via pip
 - Snip / https://github.com/dlrudie/Snip/releases/latest
 
-###### Install:
+##### Install:
 1. Install Snip and run at least once, to generate snip.txt
 2. Find your Discord Login token (See "Finding Login Token below")
 3. Update defaultconfig.ini with your token and path to snip.txt, your path to snip.txt should look something like this. `C:\Users\User\Documents\Snip\snip.txt`
 4. Rename defaultconfig.ini to config.ini **IMPORTANT**
 5. Run `run.py`
 
-###### Finding Login Token
+##### Finding Login Token
 1. While on the Discord Desktop app press `ctrl + shift + i`
 2. Switch to `console` tab
 3. type `localStorage.token` and press enter
--- run.py
+++ run.py
@@ -1,92 +1,105 @@
-import discord
-from discord.ext import commands
 import asyncio
 import configparser
 import inspect
+import sys
+import time
+import os
+
+import discord
+from discord.ext import commands
+from logbook import Logger, StreamHandler, FileHandler
+
+logger = Logger("Discord Music")
+logger.handlers.append(StreamHandler(sys.stdout, bubble=True))
+logger.handlers.append(FileHandler("last-run.log", bubble=True))
+
+logger.debug("Loading config files")
 
 config = configparser.ConfigParser()
-config.read("config.ini")
-try:
-    token = config['Config']['token']
-except KeyError:
-    print("Please assign your token in config.ini")
-    input()
-if token == "":
-    print("Please assign your token in config.ini")
-    input()
-try:
-    snip = config['Config']['snip']
-except KeyError:
-    print("Please assign your snip directory in config.ini")
-    input()
-if token == "":
-    print("Please assign your token in config.ini")
-    input()
 
-# Set's bot's desciption and prefixes in a list
-description = ""
-bot = commands.Bot(command_prefix=['musicself.'], description=description, self_bot=True)
+token = ""
+snip = ""
 
-###################
-## Startup Stuff ##
-###################
+if os.path.exists("config.ini"):
+    config.read("config.ini")
 
+    try:
+        token = config['Config']['token']
+    except KeyError:
+        logger.critical("No token found in config, please ensure that the config formatting is correct")
+        time.sleep(5)
+        exit(1)
+
+    if token == "":
+        logger.critical("No token set! Exiting")
+        time.sleep(5)
+        exit(1)
+
+    try:
+        snip = config['Config']['snip']
+    except KeyError:
+        logger.critical("No path to snip found in config, please ensure that the config formatting is correct")
+        time.sleep(5)
+        exit(1)
+
+    if snip == "":
+        logger.critical("No path to snip set! Exiting")
+        time.sleep(5)
+        exit(1)
+
+else:
+    logger.error("No config file, creating one now")
+    with open("default.config.ini") as f:
+        config = f.read()
+    with open("config.ini", 'w') as f:
+        f.write(config)
+    logger.info("Config created, please set config!")
+    time.sleep(3)
+    exit(0)
+
+logger.info("Config loaded")
+
+bot = commands.Bot(command_prefix=['m.'], self_bot=True)
 bot.remove_command('help')
+
 
 @bot.event
 async def on_ready():
-    # Outputs login data to console
-    print("---------------------------")
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print("---------------------------")
-    song = pull_song()
-    savesong = song
-    await bot.change_presence(afk=True, status=discord.Status.idle, game=discord.Game(name="\u266B {}".format(song)))
-    print("Now playing: {}".format(song.encode("ascii", "ignore").decode()))
-    while True:
+    logger.info(f"Logged in as {bot.user.name} with ID {bot.user.id}")
+    logger.info("Ready to start")
+
+
+@bot.command(name="quit")
+async def _quit():
+    await bot.say("Logging out...")
+    logger.info("Logging out")
+    await bot.logout()
+    exit(0)
+
+
+async def music_loop():
+    await bot.wait_until_ready()
+    await asyncio.sleep(1)
+    last_song = ""
+    while not bot.is_closed:
         song = pull_song()
-        if savesong != song:
-            await bot.change_presence(afk=True, status=discord.Status.idle, game=discord.Game(name="\u266B {}".format(song)))
-            savesong = song
-            print("Now playing: {}".format(song.encode("ascii", "ignore").decode()))
-        await asyncio.sleep(5)
+        if song != last_song:
+            await bot.change_presence(afk=True, status=discord.Status.invisible, game=discord.Game(name=song))
+            last_song = song
+            logger.info(f"Set Discord status to {song.encode('ascii', 'ignore').decode()}")
+        await asyncio.sleep(8)
+
 
 def pull_song():
-    file = open(snip, encoding="utf8")
-    song = file.read()
-    return song
+    with open(snip, encoding="utf8") as f:
+        return f.read()
 
-# Ping Pong
-# Testing the response of the bot
-@bot.command()
-async def ping():
-    """Pong. Test's responsiveness of bot"""
-    await bot.say("Music: Pong")
 
-@bot.command(pass_context=True, name="eval")
-async def eval_(ctx, *, code: str):
-    """Evaluates a line of code provided"""
-    code = code.strip("` ")
-    server = ctx.message.server
-    message = ctx.message
-    try:
-        result = eval(code)
-        if inspect.isawaitable(result):
-            result = await result
-    except Exception as e:
-        await bot.say("```py\nInput: {}\n{}: {}```".format(code, type(e).__name__, e))
-    else:
-        await bot.say("```py\nInput: {}\nOutput: {}\n```".format(code, result))
-    await bot.delete_message(message)
-
-##############################
-## FANCY TOKEN LOGIN STUFFS ##
-##############################
-
-print("Logging in")
 try:
+    logger.info("Logging in")
+    bot.loop.create_task(music_loop())
     bot.run(token, bot=False)
 except discord.errors.LoginFailure:
-    print("Invalid token")
+    logger.critical("Log in failed, check token!")
+    time.sleep(5)
+    exit(1)
